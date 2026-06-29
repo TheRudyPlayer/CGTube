ef59b6e2d8094b39ec486d612bc1bb775d72bb58
TheRudyPlayer <elerandomxd25@outlook.com>
2026-06-29 14:44:43 -0400

    Add verified status and banner to channel icons

--- LinkChannel.js
+++ LinkChannel.js
@@ -4,27 +4,35 @@ const CGTubeChannels = [
     id: "UCO4wdYUIuBRxZ4MRo_4C4aQ",
     handle: "@DragonDudeOfficial",
     name: "Dragon Dude",
-    icon: "https://i.postimg.cc/TwhcyHtC/channels4-profile-(7).jpg"
+    icon: "https://i.postimg.cc/TwhcyHtC/channels4-profile-(7).jpg",
+    verified: true,
+    banner: ""
 },
 
 {
     id: "UC5aLmBd_aN34pcpjTxjvEPw",
     handle: "@JrCrackOficial",
     name: "J.R CRACK",
-    icon: "https://i.postimg.cc/9QRfXYmq/channels4-profile-(10).jpg"
+    icon: "https://i.postimg.cc/9QRfXYmq/channels4-profile-(10).jpg",
+    verified: true,
+    banner: ""
 },
 
 {
     id: "UCx6ygSDNHqot_lMsDEpD-fw",
     handle: "@Den19K",
     name: "DEN19K",
-    icon: "https://i.postimg.cc/hGd5w607/channels4-profile-(6).jpg"
+    icon: "https://i.postimg.cc/hGd5w607/channels4-profile-(6).jpg",
+    verified: true,
+    banner: ""
 },
 {
     id: "UC-unh3YIkpHzRCeiZ6RRZ9A",
     handle: "@MirtHDD",
     name: "MirtHD",
-    icon: "https://i.postimg.cc/hPvhnps0/mirticon.png"
+    icon: "https://i.postimg.cc/hPvhnps0/mirticon.png",
+    verified: false,
+    banner: ""
 },
 {
     id: "UCK8HhT4BypRtaycRMmQD8lQ",
